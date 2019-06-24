d948cd6736e7cf0c0dc78277fcc17d0cba0240ed
JJudge <jjudge354@gmail.com>
2019-06-24 02:44:27 +0000

    added boardCanvas

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,16 +1,44 @@
-var Dracula = require('graphdracula')
-var Graph = Dracula.Graph
-var Renderer = Dracula.Renderer.Raphael
-var Layout = Dracula.Layout.Spring
 
-var graph = new Graph()
 
-graph.addEdge('Banana','Apple')
-graph.addEdge('Apple','Kiwi')
-graph.addEdge('Apple','Dragonfruit')
-graph.addEdge('Banana','Dragonfruit')
-graph.addEdge('Banana','Kiwi')
 
-//var layout = new Layout(graph)
-//var renderer = new Renderer('#paper', graph, 400,300)
-//renderer.draw()
+class Loc {
+    /* pixel location */
+
+    constructor(i,j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    loc_equals(loc2) {
+        /* is LOC equivalent to arg LOC2? */
+        return ((this.i == loc2.i) && (this.j == loc2.j));
+    }
+}
+
+
+
+function get_click_location(canvas, event) {
+    var bounds = canvas.getBoundingClientRect();
+    var x = event.clientX - bounds.left;
+    var y = event.clientY - bounds.top;
+    var loc = new Loc(x,y);
+    return loc;
+}
+
+/*
+function printReadout(message) {
+    var readout = document.getElementById('readout');
+    readout.innerHTML = message;
+}*/
+
+class Graph {
+    constructor(screenSize, adjacencyMatrix) {
+        this.screenSize = screenSize
+        this.adjMat = adjacencyMatrix
+    }
+
+    handleClick(clickPxLoc) {
+    }
+
+
+}
